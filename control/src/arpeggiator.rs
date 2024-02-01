3d7a4c4f49cfb1663eb899ba9c092313b93b487d
Petr Horacek <petr@zlosynth.com>
2024-02-01 23:07:03 +0100

    Add initializer for arp
    Signed-off-by: Petr Horacek <petr@zlosynth.com>

--- control/src/arpeggiator.rs
+++ control/src/arpeggiator.rs
@@ -29,3 +29,31 @@ pub enum State {
 }
 
 pub type Progression = Vec<i16, 7>;
+
+impl Arpeggiator {
+    fn new(scale: Scale, root: ScaleNote, chord: Chord) -> Self {
+        Self {
+            scale,
+            root,
+            chord,
+            mode: Mode::Up,
+            state: State::Up(0),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::scales::quarter_tones::QuarterTone;
+    use crate::scales::tonic::Tonic;
+
+    use super::*;
+
+    #[test]
+    fn initialize() {
+        let scale = Scale::new(Tonic::C, &[24]).unwrap();
+        let root = ScaleNote::new(QuarterTone::C1, 0);
+        let chord = Chord::from_slice(&[0, 1]).unwrap();
+        let _arp = Arpeggiator::new(scale, root, chord);
+    }
+}
