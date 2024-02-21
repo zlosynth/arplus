fd3bc6371156a64c1e29e5f11ea70835e5e10669
Petr Horacek <petr@zlosynth.com>
2024-02-22 00:14:01 +0100

    Recover previous chord group from save
    Signed-off-by: Petr Horacek <petr@zlosynth.com>

--- control/src/parameters/mod.rs
+++ control/src/parameters/mod.rs
@@ -28,7 +28,7 @@ pub struct Parameters {
 pub struct PersistentConfig {
     pub tone: DiscretePersistentConfig,
     pub chord: DiscretePersistentConfig,
-    pub size: DiscretePersistentConfig,
+    pub chord_group: DiscretePersistentConfig,
     pub scale: TogglePersistentConfig,
     pub mode: TogglePersistentConfig,
     pub arp: TogglePersistentConfig,
@@ -37,15 +37,20 @@ pub struct PersistentConfig {
 impl Parameters {
     pub fn new(config: PersistentConfig, chords: &Chords) -> Self {
         // TODO: Consider selected chord group too. Recovered from save Discrete attribute.
+        let chord_group_parameter =
+            Discrete::new(config.chord_group, chords.number_of_groups(), 0.1);
+
+        let selected_chord_group = chord_group_parameter.selected_value();
         // TODO: No unwrap or safety note
-        let number_of_chord_groups = chords.number_of_groups();
-        let number_of_chords_in_the_group = chords.number_of_chords(0).unwrap();
+        let number_of_chords_in_the_group = chords.number_of_chords(selected_chord_group).unwrap();
+        let chord_parameter = Discrete::new(config.chord, number_of_chords_in_the_group, 0.1);
+
         Self {
             // TODO: Set proper ranges
             // TODO: Allow configuration of tonic
             tone: Discrete::new(config.tone, 7 * 6, 0.1),
-            chord: Discrete::new(config.chord, number_of_chords_in_the_group, 0.1),
-            chord_group: Discrete::new(config.chord, number_of_chord_groups, 0.1),
+            chord: chord_parameter,
+            chord_group: chord_group_parameter,
             contour: Continuous::new(),
             cutoff: Continuous::new(),
             resonance: Continuous::new(),
@@ -60,7 +65,7 @@ impl Parameters {
         PersistentConfig {
             tone: self.tone.copy_config(),
             chord: self.chord.copy_config(),
-            size: self.chord_group.copy_config(),
+            chord_group: self.chord_group.copy_config(),
             scale: self.scale.copy_config(),
             mode: self.mode.copy_config(),
             arp: self.arp.copy_config(),
