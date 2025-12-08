80ac04376f5d2aa3798d46605d642965723dfd59
Petr Horacek <petr@zlosynth.com>
2025-12-08 23:11:16 +0100

    Fix the update_website script
    Signed-off-by: Petr Horacek <petr@zlosynth.com>

--- hack/update_website.sh
+++ hack/update_website.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 make manual/user
 make manual/build
 
-cp manual/user/manual.pdf ../zlosynth.github.io/docs/arplus-user-manual.pdf
+cp manual/user/Manual.pdf ../zlosynth.github.io/docs/arplus-user-manual.pdf
 cp manual/build/manual.pdf ../zlosynth.github.io/docs/arplus-build-manual.pdf
